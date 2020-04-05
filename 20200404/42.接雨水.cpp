0d9cfc0bc04a52bd1ea397086623c77b5efbdbbf
lingtan <xjtulim@126.com>
2020-04-05 23:46:11 +0800

    20200405_lingtan

---
+++ 20200404/42.接雨水.cpp
@@ -0,0 +1,41 @@
+/*
+ * File: 42.接雨水.cpp
+ * Project: 20200404
+ * File Created: Sunday, 5th April 2020 11:44:04 pm
+ * Author: lingtan (<<danling@stu.xjtu.edu.cn>>)
+ * Last Modified: Sunday, 5th April 2020 11:45:06 pm
+ * Modified By: lingtan (<<danling@stu.xjtu.edu.cn>>)
+ * Copyright 2017 - 2020 Your Company, <<xjtu>>
+ */
+/*
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+示例:
+输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+输出: 6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/trapping-rain-water
+*/
+class Solution {
+public:
+    int trap(vector<int>& height)
+{
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
+        }
+        st.push(current++);
+    }
+    return ans;
+}
+};
